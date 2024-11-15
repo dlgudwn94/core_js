0dea6242c02a918ee34cca16d981b9fd84b901b9
dlgudwn94 <dlgudwn82@naver.com>
2024-11-15 11:35:23 +0900

    learn. core javascript
    - manipulation
    - attr
    - style
    - css

--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -1,7 +1,9 @@
 function getNode(node, context = document) {
-  if (context.nodeType !== 9) context = getNode(context);
+  if (context.nodeType !== 9) context = document.querySelector(context);
   return context.querySelector(node);
 }
+
+getNode('.about');
 
 function getNodes(node, context = document) {
   if (context.nodeType !== 9) context = getNode(context);
